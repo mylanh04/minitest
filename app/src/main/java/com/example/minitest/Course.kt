c37d9f53d65a26d1e84de39dd97f7c1940fe9bf2
My Lanh <ngomylanh1804@gmail.com>
2024-05-09 16:28:16 +0700

    first commit

---
+++ app/src/main/java/com/example/minitest/Course.kt
@@ -0,0 +1,11 @@
+package com.example.minitest
+
+data class Course(// on below line creating variables.
+    var courseID: String? = "",
+    var courseName: String? = "",
+    var courseType: String? = "",
+    var coursePrice: String? = "",
+    var courseImage: String?= ""
+
+
+)
